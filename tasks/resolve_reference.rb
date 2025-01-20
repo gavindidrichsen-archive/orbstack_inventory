aaf38302559bae14acf0f613b53199984ebd3ec5
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2025-01-20 03:08:40 +0000

    Refactor the orbstack inventory code from the plugin
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- tasks/resolve_reference.rb
+++ tasks/resolve_reference.rb
@@ -1,20 +1,18 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'json'
+require 'orbstack_bolt_inventory'
 
 params = JSON.parse(STDIN.read)                                 # Read the input from Bolt
-installdir = params['_installdir']                              # metaparameter received by default from bolt
 group_patterns = params['group_patterns']
-lib_path = File.join(installdir, 'orbstack_inventory', 'lib')
-$LOAD_PATH.unshift(lib_path)                                    # Add the lib directory to the load path
 
-require 'orbstack_inventory'
+inventory = OrbstackBoltInventory::Inventory.new({ 'group_patterns' => group_patterns })
 
-inventory = OrbstackInventory.new({ 'group_patterns' => group_patterns })
-
-# Call the resolve_reference method and output the result
+# Generate the inventory and output the result
 begin
-  result = inventory.resolve_reference
+  # Wrap the inventory result in a 'value' key as expected by Bolt
+  result = { 'value' => inventory.generate }
   puts result.to_json
 rescue StandardError => e
   STDERR.puts({ _error: { msg: e.message, kind: 'bolt/plugin-error' } }.to_json)
